91fc50fefc7b92774c8d22ce7ec7e36a2674b012
MatzJeurissen <84962321+MatzJeurissen@users.noreply.github.com>
2023-03-18 18:52:00 +0100

    INIT

--- server/models/database.js
+++ server/models/database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true });
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
